5274b5a585365a5d3c55acfb01333795820ccfda
Itai Yosephi <itai.yosephi@gmail.com>
2018-06-20 10:01:52 +0300

    updates

--- js/offCanvas.js
+++ js/offCanvas.js
@@ -3,4 +3,6 @@ function openCanvas(){
     document.querySelector('.offcanvas-aside').classList.toggle('offcanvas-aside-open');
     document.querySelector('.freeze').classList.toggle('freeze-active');    
     document.querySelector('body').classList.toggle('no-scroll');    
+    document.querySelector('.offcanvas-btn .fa').classList.toggle('fa-comment');    
+    document.querySelector('.offcanvas-btn .fa').classList.toggle('fa-times');    
 }
